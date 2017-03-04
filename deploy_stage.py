40991f2f6665387dde6406e9acb67241ef9d01fa
U-lis <ulismoon@gmail.com>
2017-03-05 00:21:51 +0900

    Fix deploy script

--- deploy_stage.py
+++ deploy_stage.py
@@ -8,13 +8,13 @@ home = os.path.expanduser("~")
 
 with paramiko.SSHClient() as client:
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    #client.connect(hostname="54.199.224.189", username="ubuntu", key_filename=os.path.join(home, ".ssh", "id_rsa"))
-    client.connect(hostname="54.199.224.189", username="ubuntu", key_filename=os.path.join(home, "ulismoon_bravepeach.pem"))
+    client.connect(hostname="54.199.224.189", username="ubuntu", key_filename=os.path.join(home, ".ssh", "id_rsa"))
 
     cmd_dict = OrderedDict()
     cmd_dict["Install Dependency Packages"] = ["sudo apt install -y " + " ".join(APT_PACKAGE_LIST),
                                                "sudo pip3 install virtualenv"]
-    cmd_dict["Pull Latest Source"] = ["cd bravepeach_web", "git pull"]
+    cmd_dict["Pull Latest Source"] = ["cd bravepeach_web", "git pull",
+                                      "cp bravepeach/settings/example.py bravepeach/settings/staging.py"]
     cmd_dict["Setup Python Environment"] = ["cd bravepeach_web", "virtualenv -p python3 .venv",
                                             "source .venv/bin/activate", "pip install -r requirements.txt"]
     #cmd_dict["Reload Service"] = ["sudo systemd service uwsgi reload"]
