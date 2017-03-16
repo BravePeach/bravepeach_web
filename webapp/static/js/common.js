dad7678afcfd6114ea82b539162e86f493bea541
U-lis <ds2ilz@hotmail.com>
2017-03-16 23:15:37 +0900

    Check ajax call. Fix some js errors. Add greeting link for test

--- webapp/static/js/common.js
+++ webapp/static/js/common.js
@@ -13,3 +13,20 @@ function getCookie(name) {
     }
     return cookieValue;
 }
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+$(function(){
+    var csrftoken = getCookie('csrftoken');
+
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+});
