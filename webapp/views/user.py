6a21c5f3549153c228863189e7b8f481c5cc939b
U-lis <ds2ilz@hotmail.com>
2017-03-18 03:06:01 +0900

    PC mypage: alarm, cert, payment done.

--- webapp/views/user.py
+++ webapp/views/user.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.shortcuts import redirect, get_object_or_404
 from django.http import HttpResponseRedirect, JsonResponse
 from django.contrib import messages
@@ -6,7 +8,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 from ..forms import UserRegistrationForm, UserEditForm, ProfileEditForm
-from ..models import Profile
+from ..models import Profile, GuideOffer, Review
 from bravepeach.util import flavour_render
 
 
@@ -101,8 +103,40 @@ def mypage(request, page_type="account"):
     if page_type not in page_type_dict:
         return redirect("index")
 
-    return flavour_render(request, "user/mypage/"+page_type+".html",
-                          {"page_type": page_type, "type_dict": page_type_dict})
+    param_dict = {"page_type": page_type, "type_dict": page_type_dict}
+
+    if page_type == "alarm":
+        # TODO: get alarm list
+        # param_dict["alarn_list"] = alarm_list
+        pass
+    elif page_type == "account":
+        pass
+    elif page_type == "payment":
+        payment_list = (GuideOffer.objects.filter(request__user_id=request.user.id,
+                                                  request__travel_end_at__lt=datetime.date.today())
+                        .prefetch_related('request').prefetch_related('cancel').order_by('-id'))
+        wait_list = []
+        paid_list = []
+        cancel_list = []
+        for payment in payment_list:
+            if payment.is_canceled:
+                cancel_list.append(payment)
+            elif payment.paid:
+                paid_list.append(payment)
+            else:
+                wait_list.append(payment)
+        param_dict.update(**{"wait_list": wait_list, "paid_list": paid_list, "cancel_list": cancel_list})
+
+    elif page_type == "review":
+        # TODO: model change?
+        review_list = Review.objects.filter(writer=request.user).order_by('-id')
+        param_dict['review_list'] = review_list
+    elif page_type == "cert":
+        pass
+    elif page_type == 'unsub':
+        pass
+
+    return flavour_render(request, "user/mypage/"+page_type+".html", param_dict)
 
 
 @login_required
