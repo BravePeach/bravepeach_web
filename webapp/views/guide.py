83d9b19ea114e6dd71557ee7ae963a180d7a9855
U-lis <ds2ilz@hotmail.com>
2017-03-22 06:41:27 +0900

    Fix guide profile

--- webapp/views/guide.py
+++ webapp/views/guide.py
@@ -14,7 +14,5 @@ def profile(request, gid):
                    .select_related('request').order_by('-request__travel_end_at')).all()[:5]
     review_list = Review.objects.filter(receiver_id=guide.id).order_by('-id')
     return flavour_render(request, "guide/profile.html", {"guide": guide, "recent_trip": recent_trip,
-                                                          "rating": range(guide.clean_rating[0]),
-                                                          "norating": range(4-guide.clean_rating[0]),
                                                           "reviews": review_list,
                                                           "review_ids": [x.offer_id for x in review_list]})
