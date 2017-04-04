a67b23408d63e61be15c1757eb66fcc7be4d8db4
U-lis <ulismoon@gmail.com>
2017-04-04 22:22:45 +0900

    fb login wip

--- bravepeach/urls.py
+++ bravepeach/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('webapp.urls')),
+    url(r"", include('social_django.urls', namespace='social')),
     ]
 
 
