77ace610f452c594715fb8c774dfaa100bad2e27
U-lis <ulismoon@gmail.com>
2017-04-05 22:51:39 +0900

    FB login: Full feature

---
+++ webapp/static/js/social_login.js
@@ -0,0 +1,43 @@
+    window.fbAsyncInit = function () {
+        FB.init({
+            appId: '1520837081477959',
+            xfbml: true,
+            version: 'v2.8'
+        });
+        FB.AppEvents.logPageView();
+    };
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+function fb_login(){
+    FB.login(function(response){
+        if(response.status === 'connected') {
+            console.log(response);
+            $.post("/login_fb/", {"access_token": response.authResponse.accessToken},
+                function(data){
+                    console.log(data);
+                    if(data['ok'] === true) {
+                        window.location.href = '/';
+                    } else{
+                        swal({
+                            type: "error",
+                            title: "FB Login Failed",
+                            text: data["msg"]
+                        });
+                    }
+                });
+        } else {
+            console.log("no");
+        }
+    }, {scope: "email"});
+}
+
+function ggl_login(){
+
+}
