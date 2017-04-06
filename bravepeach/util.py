b4b18055c7db3056043ee33474cb82b2b2d602b5
U-lis <ds2ilz@hotmail.com>
2017-04-06 21:26:25 +0900

    Email Certification

--- bravepeach/util.py
+++ bravepeach/util.py
@@ -2,6 +2,11 @@
 Utility Functions.
 """
 import os
+import base64
+import hashlib
+
+from Crypto.Cipher import AES
+from Crypto import Random
 from django.shortcuts import render
 
 
@@ -15,3 +20,39 @@ def flavour_render(request, template_name, context=None, content_type=None, stat
     else:
         template_name = os.path.join("pc", template_name)
     return render(request, template_name, context, content_type, status, using)
+
+
+class AESCipher(object):
+    """
+    A classical AES Cipher. Can use any size of data and any size of password thanks to padding.
+    Also ensure the coherence and the type of the data with a unicode to byte converter.
+    """
+    def __init__(self, key):
+        self.bs = 32
+        self.key = hashlib.sha256(AESCipher.str_to_bytes(key)).digest()
+
+    @staticmethod
+    def str_to_bytes(data):
+        u_type = type(b''.decode('utf8'))
+        if isinstance(data, u_type):
+            return data.encode('utf8')
+        return data
+
+    def _pad(self, s):
+        return s + (self.bs - len(s) % self.bs) * AESCipher.str_to_bytes(chr(self.bs - len(s) % self.bs))
+
+    @staticmethod
+    def _unpad(s):
+        return s[:-ord(s[len(s)-1:])]
+
+    def encrypt(self, raw):
+        raw = self._pad(AESCipher.str_to_bytes(raw))
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + cipher.encrypt(raw)).decode('utf-8')
+
+    def decrypt(self, enc):
+        enc = base64.b64decode(enc)
+        iv = enc[:AES.block_size]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
